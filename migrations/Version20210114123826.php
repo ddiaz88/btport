fae8d9b0e9fa42e92c821aa8f4e6b1d9014e7af2
ddiaz88 <71702472+ddiaz88@users.noreply.github.com>
2021-01-14 14:25:03 +0100

    Add files via upload

---
+++ migrations/Version20210114123826.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210114123826 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE nombre (id INT AUTO_INCREMENT NOT NULL, idcancion VARCHAR(50) NOT NULL, album VARCHAR(50) NOT NULL, autor VARCHAR(50) NOT NULL, genero VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE techno (id INT AUTO_INCREMENT NOT NULL, nombre VARCHAR(75) NOT NULL, autor VARCHAR(50) NOT NULL, productora VARCHAR(100) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE techyes (id INT AUTO_INCREMENT NOT NULL, nombre VARCHAR(50) NOT NULL, autor VARCHAR(50) NOT NULL, productora VARCHAR(50) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE nombre');
+        $this->addSql('DROP TABLE techno');
+        $this->addSql('DROP TABLE techyes');
+    }
+}
